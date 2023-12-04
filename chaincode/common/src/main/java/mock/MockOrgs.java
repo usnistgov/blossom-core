b60adc32f8cde8134459c136ec29a23a0d0aa52f
joshua-roberts <joshua.roberts@nist.gov>
2023-12-04 08:47:19 -0500

    remove common folder

--- chaincode/common/src/main/java/mock/MockOrgs.java
+++
@@ -1,9 +0,0 @@
-package mock;
-
-public class MockOrgs {
-
-    public static final String ORG1_MSP = "Org1MSP";
-    public static final String ORG2_MSP = "Org2MSP";
-    public static final String ORG3_MSP = "Org3MSP";
-
-}
